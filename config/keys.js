e384b4eda8a7fa605075e5c234c1fdca813cec9d
Abhay Tripathi <abhaytripathi070@gmail.com>
2022-07-21 22:35:24 +0530

    removed my credentials

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 const expressEjsLayouts = require("express-ejs-layouts");
 
-dbPassword = "mongodb+srv://Drago:Bk8UN2P5a4s4RD5B@cluster0.5avif.mongodb.net/?retryWrites=true&w=majority";
+dbPassword = "mongodb+srv://Username:UserPassword@cluster0.5avif.mongodb.net/?retryWrites=true&w=majority";
 module.exports ={
     mongoURI: dbPassword
 };
