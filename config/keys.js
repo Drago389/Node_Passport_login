2d75249f19c5a2b795fb173c41539e74c3185e82
Drago389 <abhaytripathi070@gmail.com>
2022-07-21 21:52:53 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+const expressEjsLayouts = require("express-ejs-layouts");
+
+dbPassword = "mongodb+srv://Drago:Bk8UN2P5a4s4RD5B@cluster0.5avif.mongodb.net/?retryWrites=true&w=majority";
+module.exports ={
+    mongoURI: dbPassword
+};
+
